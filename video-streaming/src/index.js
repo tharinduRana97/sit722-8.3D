94d13dc8582ebd3ac5dccf95007865c465eae3f6
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-13 00:02:35 +1000

    Update index.js for video-streaming

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT;
 const RABBIT = process.env.RABBIT;
 
 // Application entry point.
-//
 async function main() {
     const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
     
